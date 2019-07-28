96c2084ac06ac1ebf33f2b954f6e30cce612a021
terekhin <nick.terekhin@gmail.com>
2019-07-29 02:12:29 +0300

    improved child theme styled

---
+++ functions.php
@@ -0,0 +1,37 @@
+<?php
+
+define("CHILD_THEME_PATH_URI",get_stylesheet_directory_uri());
+define("CHILD_THEME_PATH",get_stylesheet_directory());
+define("CHILD_THEME_MAIN_STYLE",get_stylesheet_uri());
+define("CHILD_THEME_UPLOAD_URI",wp_upload_dir()['baseurl']);
+
+add_action('wp_enqueue_scripts', 'main_style_setup',20);
+function main_style_setup()
+{
+
+
+    wp_register_style( 'custom-css', CHILD_THEME_PATH_URI.'/content/css/custom.css?13');
+    wp_register_script( 'custom-js', CHILD_THEME_PATH_URI . '/content/js/custom.js?13');
+
+
+    wp_enqueue_style( 'custom-css' );
+    wp_enqueue_script( 'custom-js' );
+}
+
+
+
+//add_filter( 'use_block_editor_for_post', '__return_false' );
+add_filter('show_admin_bar', '__return_false');
+
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_script('jquery-mask-plugin', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js', array(), '1.14.15', true);
+});
+
+add_filter('quform_element_valid_1_5', function ($valid, $value, Quform_Element_Field $element) {
+    if ( ! preg_match('/^\(\d{3}\) \d{3}\-\d{4}$/', $value)) {
+        $element->addError('Введите номер телефона в формате (000) 000-0000');
+        $valid = false;
+    }
+
+    return $valid;
+}, 10, 3);
