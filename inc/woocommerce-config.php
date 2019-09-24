891e1d7bbeeb2b623f8244357e15a39738740cbe
terekhin <nick.terekhin@gmail.com>
2019-09-25 00:51:52 +0300

    restyled catalog

--- inc/woocommerce-config.php
+++ inc/woocommerce-config.php
@@ -51,3 +51,52 @@ function _td_add_to_cart_text()
 {
     return 'В корзину';
 }
+
+
+function woocommerce_catalog_ordering() {
+    if ( ! wc_get_loop_prop( 'is_paginated' ) || ! woocommerce_products_will_display() ) {
+        return;
+    }
+    $show_default_orderby = 'menu_order' === apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby', 'menu_order' ) );
+    $catalog_orderby_options = apply_filters(
+        'woocommerce_catalog_orderby',
+        array(
+            'menu_order' => __( 'По умолчанию', 'woocommerce' ),
+            'popularity' => __( 'по популярности', 'woocommerce' ),
+            'rating'     => __( 'По рейтингу', 'woocommerce' ),
+            'date'       => __( 'Последние', 'woocommerce' ),
+            'price'      => __( 'Щт дещевых к дорогим', 'woocommerce' ),
+            'price-desc' => __( 'От дорогих к дешевым', 'woocommerce' ),
+        )
+    );
+
+    $default_orderby = wc_get_loop_prop( 'is_search' ) ? 'relevance' : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby', '' ) );
+    $orderby         = isset( $_GET['orderby'] ) ? wc_clean( wp_unslash( $_GET['orderby'] ) ) : $default_orderby; // WPCS: sanitization ok, input var ok, CSRF ok.
+
+    if ( wc_get_loop_prop( 'is_search' ) ) {
+        $catalog_orderby_options = array_merge( array( 'relevance' => __( 'Релевнтные', 'woocommerce' ) ), $catalog_orderby_options );
+
+        unset( $catalog_orderby_options['menu_order'] );
+    }
+
+    if ( ! $show_default_orderby ) {
+        unset( $catalog_orderby_options['menu_order'] );
+    }
+
+    if ( ! wc_review_ratings_enabled() ) {
+        unset( $catalog_orderby_options['rating'] );
+    }
+
+    if ( ! array_key_exists( $orderby, $catalog_orderby_options ) ) {
+        $orderby = current( array_keys( $catalog_orderby_options ) );
+    }
+
+    wc_get_template(
+        'loop/orderby.php',
+        array(
+            'catalog_orderby_options' => $catalog_orderby_options,
+            'orderby'                 => $orderby,
+            'show_default_orderby'    => $show_default_orderby,
+        )
+    );
+}
